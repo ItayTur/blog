f80fde4a33861841ef8d559c4d6e8c4dcbb598ba
Itay Tur <42403535+ItayTur@users.noreply.github.com>
2022-01-03 20:32:35 +0200

    ingress config and client deployment

--- client/src/components/CommentForm/CommentForm.js
+++ client/src/components/CommentForm/CommentForm.js
@@ -7,7 +7,7 @@ const CommentForm = ({ postId }) => {
   const onSubmit = async (e) => {
     e.preventDefault();
     const { data } = await axios.post(
-      `http://localhost:4001/posts/${postId}/comments`,
+      `http://posts.com/posts/${postId}/comments`,
       { content }
     );
     console.log(data);
